7e29e5a6e00781f967c213b28c67ba5150a3d846
Ibrahima Kuete <delano@MBPvonIbrahima2.fritz.box>
2022-11-20 22:32:25 +0100

    add test code with dummy widget

---
+++ test/flutter_pkg_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_pkg/flutter_pkg.dart';
+
+void main() {
+  test('adds one to input values', () {
+    /*final customButton = CustomButton();
+    expect(customButton.addOne(2), 3);
+    expect(customButton.addOne(-7), -6);
+    expect(customButton.addOne(0), 1);*/
+  });
+}
